ba4c4e4f4b99e168cd5724a293f7c156c8919b31
Denis Eroshevskii <devskjj@gmail.com>
2025-09-23 23:16:32 +0600

    Created enum - TypeOfGoods for Goods.class. Add some field to Goods.class.

---
+++ src/enums/TypeOfGoods.java
@@ -0,0 +1,24 @@
+package enums;
+
+public enum TypeOfGoods {
+    MEAT("Мясо"),
+    DRYFRUITS("Сухофрукты"),
+    GRAIN("Зерно"),
+    FLOUR("Мука"),
+    FABRICS("Ткани"),
+    PAINT("Краска");
+
+    private String value;
+
+    TypeOfGoods(String value) {
+        this.value=value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
---
+++ src/models/Goods.java
@@ -0,0 +1,10 @@
+package models;
+
+import enums.TypeOfGoods;
+
+public class Goods {
+    private int weight;
+    private TypeOfGoods type;
+
+    private double price;
+}
